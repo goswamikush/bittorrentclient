68092926cd0e493516688d86ca87b2525a13f161
Kush Goswami <kushvgoswami84@gmail.com>
2026-01-29 09:32:40 -0500

    Implement integer bencoding

--- utils/encoder.c
+++ utils/encoder.c
@@ -4,12 +4,17 @@
 #include "../types/tree.h"
 
 unsigned char *bencode_string(tree_node *node);
+unsigned char *bencode_int(tree_node *node);
 
 unsigned char *bencode(tree_node *node) {
     unsigned char *res;
 
     if (node->type == STR) {
         bencode_string(node);
+    }
+
+    if (node->type == INT) {
+        bencode_int(node);
     }
 
     return res;
@@ -68,16 +73,65 @@ unsigned char *bencode_string(tree_node *node) {
     return bencoded_string;
 }
 
+unsigned char *bencode_int(tree_node *node) {
+    // Int num of digits
+    int int_value = node->val.comp_int;
+
+    int int_value_copy = int_value;
+
+    int i = 0;
+    int total_length = 0;
+    while (int_value_copy > 0) {
+        int_value_copy = int_value_copy / 10;
+        total_length++;
+    }
+
+    // Allocate mem for bencoded_int
+    unsigned char *bencoded_int = malloc(total_length + 3);
+
+    // Add prefix 'i'
+    bencoded_int[0] = 'i';
+
+    // Add suffix 'e'
+    bencoded_int[total_length + 1] = 'e';
+
+    // Add null terminator
+    bencoded_int[total_length + 2] = '\0';
+
+    // Add digits in between
+    int_value_copy = int_value;
+    i = total_length;
+    while (i > 0) {
+        int curr_digit = int_value_copy % 10 + '0';
+        int_value_copy = int_value_copy / 10;
+
+        bencoded_int[i] = (char)curr_digit;
+
+        i--;
+    }
+
+    printf("Bencoded int %s\n", bencoded_int);
+
+    return bencoded_int;
+}
+
 int main() {
+    // tree_node *test_node = malloc(sizeof(tree_node));
+
+    // char test_val[6] = "hello";
+
+    // test_node->type = STR;
+    // test_node->val.comp_str = test_val;
+    // test_node->val_size = 5;
+
+    // printf("Final bencoded string: %s\n", bencode_string(test_node));
+
     tree_node *test_node = malloc(sizeof(tree_node));
 
-    char test_val[6] = "hello";
+    test_node->type = INT;
+    test_node->val.comp_int = 687;
 
-    test_node->type = STR;
-    test_node->val.comp_str = test_val;
-    test_node->val_size = 5;
-
-    printf("Final bencoded string: %s\n", bencode_string(test_node));
+    printf("Final bencoded int: %s\n", bencode_int(test_node));
     
     return 0;
 }
