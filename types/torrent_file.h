09b650b6db42c2f652a121cf1420b37167877800
Kush Goswami <kushvgoswami84@gmail.com>
2026-01-28 20:08:32 -0500

    Change pieces type to support bytes

--- types/torrent_file.h
+++ types/torrent_file.h
@@ -7,7 +7,7 @@ typedef struct {
     char *announce;
     char *info_hash;
     int piece_length;
-    char *pieces;
+    unsigned char *pieces;
     char *file_name;
     int total_size;
 } torrent_file;
